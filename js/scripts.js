65d0a41f5bb43675493849356d1028d58d8d662d
Vivvianne Kimani <ndiahav@gmail.com>
2018-07-20 11:49:59 +0300

    put buttons and forms

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//business logic
+var player1 = "";
+var player2 = "";
+var throw dice = function(){
+    return Math.floor(6 * Math.random())+1;
+}
+function Player(turn){
+    this.roll = 0;
+    this.tempscore = 0;
+    this.totalscore = 0;
+    this.turn = turn;
+    this.playername;
+}
